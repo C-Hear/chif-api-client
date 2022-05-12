b028d3d00c9c13ecdf4c4718241aadf04cbc4fbc
Jonathan Knezek <jdknezek@gmail.com>
2022-05-12 11:26:40 -0500

    Add quick encode scripts

---
+++ tools/encode-and-download-nix.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+DIRNAME=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+node $DIRNAME/../chif-api.mjs encode --manifest ${1%/}/manifest.json --chif ${1%/}.chif
---
+++ tools/encode-and-download-win.cmd
@@ -0,0 +1,2 @@
+@node %~dp0\..\chif-api.mjs encode --manifest %1\manifest.json --chif %1.chif
+pause
